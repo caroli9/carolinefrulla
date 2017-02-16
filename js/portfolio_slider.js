6abee9944cdb87414c689d8efae9a0f5ab3ffdc4
Caroline Frulla <caroli9@vt.edu>
2017-02-16 11:06:37 -0500

    Added Files

---
+++ js/portfolio_slider.js
@@ -0,0 +1,158 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+				
+		{
+			'title' : 'Brochure',
+			'description'   : 'This trifold brochure was created using Adobe InDesign and Photoshop. It was created as an assignment for Designing Documents for Print and is a redesign of a previous Virginia Tech Human Resources brochure.',
+			'thumbnail' : ['img/portfolio/small/Redesign_distressed_employee_tn.jpg', 'img/portfolio/Redesign_distressed_employee2.jpg'],
+			'large' : ['img/portfolio/Redesign_distressed_employee.jpg', 'img/portfolio/Redesign_distressed_employee2.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Misc','All']
+			},
+			
+		
+        	{
+			'title' : 'Infographic',
+			'description'   : 'This infographic was created using Adobe InDesign and Photoshop. It was created as an assignment for my Designing Documents for Print class.',
+			'thumbnail' : ['img/portfolio/small/Infographic_tn.jpg'],
+			'large' : ['img/portfolio/large/Infographic.jpg',],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Infographic','All']
+			},
+			
+			{
+			'title' : 'Business Card',
+			'description'   : 'This business card and logo were created for the Star City Brewers Guild using Adobe InDesign and Illustrator.',
+			'thumbnail' : ['img/portfolio/small/SCBG_CARD_tn.jpg'],
+			'large' : ['img/portfolio/large/SCBG_CARD.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Misc','All']
+			},
+			
+		
+			{
+			'title' : 'Invitation',
+			'description'   : 'This baby shower invitation was designed for a coed shower and was mailed to the parents-to-be&#39;s extended family with an accompanying insert.',
+			'thumbnail' : ['img/portfolio/small/Babypalooza_tn.jpg', 'img/portfolio/large/babypalooza book.jpg'],
+			'large' : ['img/portfolio/large/babypalooza.jpg', 'img/portfolio/large/babypalooza book.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Invitations','All']
+			},
+			
+			{
+			'title' : 'Invitation',
+			'description'   : 'This invitation was designed for a coed shower for the honoree&#39;s friends and family. It was mailed with an accompanying insert.',
+			'thumbnail' : ['img/portfolio/small/A BABY_tn.jpg', 'img/portfolio/a baby book.jpg'],
+			'large' : ['img/portfolio/large/A BABY.jpg', 'img/portfolio/a baby book.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Invitations','All']
+			},
+			
+			{
+			'title' : 'Invitation',
+			'description'   : 'This was created for a neighboorhood Octoberfest party.',
+			'thumbnail' : ['img/portfolio/small/octoberfest_tn.jpg'],
+			'large' : ['img/portfolio/large/octoberfest.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Invitations','All']
+			},
+
+			{
+			'title' : 'Flyer',
+			'description'   : 'This event flyer was created for a class I was taking: Designing Documents for Print. We were allowed to choose an actual event (as opposed to a made-up one), so I created a flyer for an upcoming recruitment event. This was emailed out to a listserv of potential new members and hung up around the Virginia Tech campus.',
+			'thumbnail' : ['img/portfolio/small/AOE Halloween_tn.jpg', ],
+			'large' : ['img/portfolio/large/AOE Halloween.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Event Posters','All']
+			},
+			
+			{
+			'title' : 'Flyer',
+			'description'   : 'This event poster was created for the Society of Engineering Science to advertise their bowling recruitment event. It was sent out over an email listserv.',
+			'thumbnail' : ['img/portfolio/small/SES Bowling_tn.jpg', ],
+			'large' : ['img/portfolio/large/SES Bowling.jpg',],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Event Posters','All']
+			},
+			
+			{
+			'title' : 'Flyer',
+			'description'   : 'This event poster was created for the Society of Engineering Science to advertise their game night. It was sent out over an email listserv.',
+			'thumbnail' : ['img/portfolio/small/SES Game Night_tn.jpg',],
+			'large' : ['img/portfolio/large/SES Game Night.jpg',],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Event Posters','All']
+			},
+			
+			{
+			'title' : 'Flyer',
+			'description'   : 'This event poster was created for the Society of Engineering Science to advertise an event they were hosting. It was sent out over an email listserv.',
+			'thumbnail' : ['img/portfolio/small/SES Lab Meeting Flyer_tn.jpg'],
+			'large' : ['img/portfolio/large/SES Lab Meeting Flyer.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Event Posters','All']
+			},
+			
+			{
+			'title' : 'Social Media',
+			'description'   : 'This was designed to advertise Alpha Omega Epsilon&#39;s super hero themed philanthropy. It was posted on the sorority instagram, sorority facebook, and event facebook.',
+			'thumbnail' : ['img/portfolio/small/TITTM_tn.jpg'],
+			'large' : ['img/portfolio/large/TITTM.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Social Media','All']
+			},
+			
+			{
+			'title' : 'Infographic',
+			'description'   : 'This infographic was created using Canva.com. It was created as an assignment for my Professional and Technical Writing seminar class.',
+			'thumbnail' : ['img/portfolio/small/Social Media Strategists_tn.jpg'],
+			'large' : ['img/portfolio/large/Social Media Strategists.jpg',],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Infographic','All']
+			},
+			
+			{
+			'title' : 'Social Media',
+			'description'   : 'This was designed to advertise Alpha Omega Epsilon&#39;s fall recruitment event. It was posted on the sorority instagram and facebook.',
+			'thumbnail' : ['img/portfolio/small/Fall Festival_tn.jpg',],
+			'large' : ['img/portfolio/large/Fall Festival.jpg',],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Social Media','All']
+			},
+	 
+		]
+	});
+});
